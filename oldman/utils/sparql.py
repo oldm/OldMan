f92931ef132302ba1062aedc1fbb7b4f4b452b4a
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2014-05-28 18:58:22 +0200

    ValueFormat and sparql utils documented

--- oldman/utils/sparql.py
+++ oldman/utils/sparql.py
@@ -3,8 +3,13 @@ from rdflib.plugins.stores.sparqlstore import SPARQLUpdateStore
 
 
 def parse_graph_safely(graph, *args, **kwargs):
-    """
-        Skolemizes the input source if the graph use a SPARQLUpdateStore
+    """Skolemizes the input source if the graph uses a
+    :class:`rdflib.plugins.stores.sparqlstore.SPARQLUpdateStore` object.
+
+    :param graph: :class:`rdflib.graph.Graph` object.
+    :param args: Argument `list` to transmit to :func:`rdflib.graph.Graph.parse`.
+    :param kwargs: Argument `dict` to transmit to :func:`rdflib.graph.Graph.parse`.
+    :return: The updated :class:`rdflib.graph.Graph` object.
     """
     if isinstance(graph.store, SPARQLUpdateStore):
         g = Graph().parse(*args, **kwargs).skolemize()
@@ -15,6 +20,13 @@ def parse_graph_safely(graph, *args, **kwargs):
 
 
 def build_query_part(verb_and_vars, subject_term, lines):
+    """Builds a SPARQL query.
+
+    :param verb_and_vars: SPARQL verb and variables.
+    :param subject_term: Common subject term.
+    :param lines: Lines to insert into the WHERE block.
+    :return: A SPARQL query.
+    """
     if len(lines) == 0:
         return ""
     query_part = u'%s { \n%s } \n' % (verb_and_vars, lines)
@@ -24,4 +36,11 @@ def build_query_part(verb_and_vars, subject_term, lines):
 
 
 def build_update_query_part(verb, subject, lines):
+    """Builds a SPARQL Update query.
+
+    :param verb: SPARQL verb.
+    :param subject: Common subject term.
+    :param lines: Lines to insert into the WHERE block.
+    :return: A SPARQL Update query.
+    """
     return build_query_part(verb, u"<%s>" % subject, lines)
--- oldman/validation/value_format.py
+++ oldman/validation/value_format.py
@@ -36,68 +36,82 @@ class AnyValueFormat(ValueFormat):
     """Accepts any value."""
 
     def check_value(self, value):
-        """Accepts any value."""
+        """See :func:`oldman.validation.value_format.ValueFormat.check_value`."""
         pass
 
 
 class TypedValueFormat(ValueFormat):
+    """Checks that the value is of a given type.
+
+    :param types: Supported Python types.
+    """
 
     def __init__(self, types):
         self._types = types
 
     def check_value(self, value):
+        """See :func:`oldman.validation.value_format.ValueFormat.check_value`."""
         if not isinstance(value, self._types):
             raise ValueFormatError(u"%s is not a %s" % (value, self._types))
 
 
 class IRIValueFormat(ValueFormat):
+    """Checks that the value is an IRI."""
 
     def check_value(self, value):
+        """See :func:`oldman.validation.value_format.ValueFormat.check_value`."""
         #TODO: to be implemented
         pass
 
 
 class PositiveTypedValueFormat(TypedValueFormat):
+    """Checks that the value is a positive number."""
 
     def check_value(self, value):
+        """See :func:`oldman.validation.value_format.ValueFormat.check_value`."""
         TypedValueFormat.check_value(self, value)
         if value <= 0:
             raise ValueFormatError(u"%s should be positive" % value)
 
 
 class NegativeTypedValueFormat(TypedValueFormat):
+    """Checks that the value is a negative number."""
 
     def check_value(self, value):
+        """See :func:`oldman.validation.value_format.ValueFormat.check_value`."""
         TypedValueFormat.check_value(self, value)
         if value >= 0:
             raise ValueFormatError(u"%s should be negative" % value)
 
 
 class NonPositiveTypedValueFormat(TypedValueFormat):
+    """Checks that the value is a non-positive number."""
 
     def check_value(self, value):
+        """See :func:`oldman.validation.value_format.ValueFormat.check_value`."""
         TypedValueFormat.check_value(self, value)
         if value > 0:
             raise ValueFormatError(u"%s should not be positive" % value)
 
 
 class NonNegativeTypedValueFormat(TypedValueFormat):
+    """Checks that the value is a non-negative number."""
 
     def check_value(self, value):
+        """See :func:`oldman.validation.value_format.ValueFormat.check_value`."""
         TypedValueFormat.check_value(self, value)
         if value < 0:
             raise ValueFormatError(u"%s should not be negative" % value)
 
 
 class HexBinaryFormat(TypedValueFormat):
-    """
-        Numbers should ALREADY be encoded as hexadecimal strings
-    """
+    """Checks that the value is a hexadecimal string."""
 
     def __init__(self):
         TypedValueFormat.__init__(self, (str, unicode))
 
     def check_value(self, value):
+        """See :func:`oldman.validation.value_format.ValueFormat.check_value`."""
         TypedValueFormat.check_value(self, value)
         try:
             int(value, 16)
@@ -105,18 +119,18 @@ class HexBinaryFormat(TypedValueFormat):
             raise ValueFormatError(u"%s is not a hexadecimal value" % value)
 
     def to_python(self, rdf_term):
-        """
-            Returns an hexstring (not unhexlified bytes)
-        """
+        """Returns a hexstring."""
         return unicode(rdf_term)
 
 
 class EmailValueFormat(TypedValueFormat):
+    """Checks that the value is an email address."""
 
     def __init__(self):
         TypedValueFormat.__init__(self, (str, unicode))
 
     def check_value(self, value):
+        """See :func:`oldman.validation.value_format.ValueFormat.check_value`."""
         # Check that it is a string
         TypedValueFormat.check_value(self, value)
 
