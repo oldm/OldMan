660e1003b8c29911b0e874ce4466cb3d7f1e05d7
Benjamin Cogrel <benjamin.cogrel@bcgl.fr>
2015-08-15 10:14:15 +0200

    HttpStore quickly updated.

--- oldman/store/http.py
+++ oldman/store/http.py
@@ -14,20 +14,16 @@ class HttpStore(Store):
         Read only. No search feature.
     """
 
-    def __init__(self, schema_graph=None, cache_region=None, session=None):
+    def __init__(self, schema_graph=None, cache_region=None, http_session=None):
         Store.__init__(self, StoreModelManager(schema_graph=schema_graph), cache_region)
-        self._session = session if session is not None else requests.session()
+        self._http_session = http_session if http_session is not None else requests.session()
         self._logger = getLogger(__name__)
 
-    @property
-    def session(self):
-        return self._session
-
-    def _get_by_iri(self, iri):
-        r = self._session.get(iri, headers=dict(Accept='text/turtle;q=1.0, '
-                                                      'application/rdf+xml;q=1.0, '
-                                                      'application/ld+json;q=.0.8, '
-                                                      'application/json;q=0.1'))
+    def _get_by_iri(self, iri, store_session):
+        r = self._http_session.get(iri, headers=dict(Accept='text/turtle;q=1.0, '
+                                                            'application/rdf+xml;q=1.0, '
+                                                            'application/ld+json;q=.0.8, '
+                                                            'application/json;q=0.1'))
         if r.status_code != 200:
             self._logger.warn("Resource %s not retrieved (Status code: %d)." % (iri, r.status_code))
             return None
@@ -55,4 +51,4 @@ class HttpStore(Store):
                 self._logger.warn("Content-type %s is not supported. Impossible to get %s." % (content_type, iri))
                 return None
 
-        return self._new_resource_object(iri, resource_graph)
+        return self._new_resource_object(iri, resource_graph, store_session)
